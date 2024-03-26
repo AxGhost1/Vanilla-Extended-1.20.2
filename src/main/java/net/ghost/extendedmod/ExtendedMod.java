2532ab6b854aad9f75fa8a9a24734b09a6f0d285
AxGhost1 <axghost420@gmail.com>
2024-03-26 22:40:26 +0530

    Important Changes

--- src/main/java/net/ghost/extendedmod/ExtendedMod.java
+++ src/main/java/net/ghost/extendedmod/ExtendedMod.java
@@ -21,7 +21,7 @@ public class ExtendedMod {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "extendedmod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public ExtendedMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
