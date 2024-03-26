97654e67ab902404f16bc1ab4aa1d8966b02d40c
AxGhost1 <axghost420@gmail.com>
2024-03-26 22:50:12 +0530

    Important Changes

--- src/main/java/net/ghost/extendedmod/ExtendedMod.java
+++ src/main/java/net/ghost/extendedmod/ExtendedMod.java
@@ -1,6 +1,7 @@
 package net.ghost.extendedmod;
 
 import com.mojang.logging.LogUtils;
+import net.ghost.extendedmod.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -25,6 +26,8 @@ public class ExtendedMod {
 
     public ExtendedMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/ghost/extendedmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.ghost.extendedmod.item;
+
+import net.ghost.extendedmod.ExtendedMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ExtendedMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
