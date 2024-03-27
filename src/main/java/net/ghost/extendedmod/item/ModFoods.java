bcf215dd48d02dd58c6a96ef1ccc4c843866473a
AxGhost1 <axghost420@gmail.com>
2024-03-27 12:03:00 +0530

    Food Items are here!!

--- src/main/java/net/ghost/extendedmod/item/ModFoods.java
+++ src/main/java/net/ghost/extendedmod/item/ModFoods.java
@@ -5,6 +5,6 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(5).fast()
+    public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(10).fast()
             .saturationMod(1f).effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 200), 50.0f).build();
 }
